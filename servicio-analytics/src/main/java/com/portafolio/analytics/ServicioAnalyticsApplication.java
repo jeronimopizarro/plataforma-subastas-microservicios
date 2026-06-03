2575991a11ea7fff531da97104712bc6edb90ded
Jero <jeronimo1080@hotmail.com>
2026-06-03 17:39:53 -0300

    fix(seguridad): ocultar clave de correo usando variables de entorno (.env)

---
+++ servicio-analytics/src/main/java/com/portafolio/analytics/ServicioAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAnalyticsApplication.class, args);
+	}
+
+}
---
+++ servicio-analytics/src/test/java/com/portafolio/analytics/ServicioAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
